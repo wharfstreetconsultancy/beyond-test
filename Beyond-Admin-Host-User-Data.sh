a98dbe00b6db5c72346cf3661af8e2951d8042a6
wharfstreetconsultancy <30436621+wharfstreetconsultancy@users.noreply.github.com>
2017-07-25 13:24:16 +0100

    Initial commit.

---
+++ Beyond-Admin-Host-User-Data.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+#
+# Install docker
+#
+sudo yum update -y
+sudo yum install -y docker
+sudo service docker start
+sudo usermod -a -G docker ec2-user
+
+
+#
+# Install kubernetes
+#
+
+# 'kubectl' module
+curl -LO https://storage.googleapis.com/kubernetes-release/release/v1.7.2/bin/linux/amd64/kubectl
+chmod +x ./kubectl
+sudo mv ./kubectl /usr/local/bin/kubectl
+
+# 'kops' module
+wget https://github.com/kubernetes/kops/releases/download/1.6.1/kops-linux-amd64
+chmod +x kops-linux-amd64
+sudo mv kops-linux-amd64 /usr/local/bin/kops
+
+#
+# Install git
+#
+sudo yum install git -y
