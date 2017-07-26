c3b28080156381ce2484861363dca13632c43de6
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-07-26 08:04:11 +0100

    Reorganised folder structure.

--- create-aws-installation.sh
+++
@@ -1,3 +0,0 @@
-﻿$env:AWS_DEFAULT_PROFILE="default"; "AWS_DEFAULT_PROFILE: "+$env:AWS_DEFAULT_PROFILE
-$env:KUBERNETES_PROVIDER="aws"
-Invoke-WebRequest -OutFile - -PassThru https://get.k8s.io | cmd
