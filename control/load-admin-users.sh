0cf17259403ba44287660d161c50f742e051e936
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-07-29 15:04:27 +0100

    Edits to load-admin-users.sh...

--- control/load-admin-users.sh
+++ control/load-admin-users.sh
@@ -9,7 +9,7 @@ aws configure --profile wsc-root
 #
 # Fetch list of admin users
 #
-export ADMIN_USERS=$(aws dynamodb scan --table-name BeyondAdminUsers | jq -r '.Items[] | .UserName.S+"="+.PublicKey.S+"\n"')
+export ADMIN_USERS=$(aws dynamodb scan --table-name BeyondAdminUsers | jq -r '.Items[] | .UserName.S+"="+.PublicKey.S+"\\n"')
 
 echo $ADMIN_USERS
 
