cf2afabe5237f70294036dcb459c2a3dd27ebd35
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-08-01 23:37:11 +0100

    Create installation process now gets reusable delegation set of resource records from pre-existing root-domain for reuse with newly-created sub-domain..

--- control/create-installation.sh
+++ control/create-installation.sh
@@ -6,9 +6,16 @@
 
 # getChangeStatus: Gets the status of the latest change
 function getChangeStatus {
-        export CHANGE_STATUS=$(aws route53 get-change --id $1 --query ChangeInfo.Status)
+        export CHANGE_STATUS=$(aws route53 get-change --profile $USER --id $1 --query ChangeInfo.Status)
         export CHANGE_STATUS=$(echo $CHANGE_STATUS | tr -d "\"")
 }
+
+#
+# Prompt user for AWS access key details (to run 'aws' cli) and other account defaults
+#
+echo "Enter AWS access key details (to run 'aws' cli) and other account defaults"
+echo "(Hint: AWS access key can be downloaded once from AWS console)"
+aws configure --profile $USER
 
 #
 # Create hosted zone for the cluster
@@ -17,43 +24,48 @@ function getChangeStatus {
 # Get current timestamp
 export TIMESTAMP=$(date +%Y%m%d%H%M%S)
 
-# Create hosted zone
-export DOMAIN=$TIMESTAMP.wharfstreetconsultancy.com
-echo Creating domain: $DOMAIN
-export CHANGE_ID=$(aws route53 create-hosted-zone --name $DOMAIN --caller-reference $TIMESTAMP --hosted-zone-config Comment="Hosted zone created by Beyond Admin user" --query ChangeInfo.Id)
-export CHANGE_ID=$(echo $CHANGE_ID | tr -d "\"")
-echo $CHANGE_ID
+# Define the root-domain for this deployment
+export ROOT_DOMAIN=wharfstreetconsultancy.com
+
+# Define the sub-domain for this deployment
+export SUB_DOMAIN=$TIMESTAMP.$ROOT_DOMAIN
+echo Creating domain: $SUB_DOMAIN
+
+# Get root-domain ID
+export ROOT_DOMAIN_ID=$(aws route53 list-hosted-zones-by-name --profile $USER --dns-name $ROOT_DOMAIN | jq -r '.HostedZones[] | select(.Name == "$ROOT_DOMAIN.") | .Id | split("/") | .[2]')
+echo ID of root domain: $ROOT_DOMAIN_ID
+
+# Get resource record set for root-domain
+export RECORD_SET_ID=$(aws route53 get-hosted-zone --profile $USER --id ROOT_DOMAIN_ID | jq -r '.DelegationSet.Id | split("/") | .[2]')
+echo ID of root domain record set: $RECORD_SET_ID
+
+# Create hosted zone for sub-domain
+# export CHANGE_ID=$(aws route53 create-hosted-zone --profile $USER --name $SUB_DOMAIN --caller-reference $TIMESTAMP --delegation-set-id $RECORD_SET_ID --hosted-zone-config Comment="Hosted zone created by Beyond Admin user" --query ChangeInfo.Id)
+# export CHANGE_ID=$(echo $CHANGE_ID | tr -d "\"")
+# echo $CHANGE_ID
 
 # Wait for hosted zone to propagate across all DNS servers
-until [ "$CHANGE_STATUS" == "INSYNC" ]; do
-        getChangeStatus $CHANGE_ID
-        echo Status is still $CHANGE_STATUS
-        sleep 5
-done
-
-# TODO - Update sub-domain hosted zone with root-domain NS records...
+# until [ "$CHANGE_STATUS" == "INSYNC" ]; do
+#         getChangeStatus $CHANGE_ID
+#         echo Status is still $CHANGE_STATUS
+#         sleep 5
+# done
 
 #
 # Create cluster records in S3
 #
-export BUCKET=cluster.$DOMAIN
-echo Creating bucket: $BUCKET
-aws s3 mb s3://$BUCKET
-export KOPS_STATE_STORE=s3://$BUCKET
+# export BUCKET=cluster.$SUB_DOMAIN
+# echo Creating bucket: $BUCKET
+# aws s3 mb s3://$BUCKET --profile $USER
+# export KOPS_STATE_STORE=s3://$BUCKET
 
 #
 # Create Kubernetes cluster
 #
 
-# TODO - Secure access to these AMI credentials
-#####
-export AWS_ACCESS_KEY_ID=???
-export AWS_SECRET_ACCESS_KEY=???
-#####
-
 # Configure cluster
-kops create cluster --ssh-public-key ~/.ssh/authorized_keys --zones=us-west-2a $DOMAIN
+# kops create cluster --ssh-public-key ~/.ssh/authorized_keys --zones=us-west-2a $SUB_DOMAIN
 
 # Build cluster
-kops update cluster --ssh-public-key ~/.ssh/authorized_keys --zones=us-west-2a $DOMAIN
+# kops update cluster --ssh-public-key ~/.ssh/authorized_keys --zones=us-west-2a $SUB_DOMAIN
 
