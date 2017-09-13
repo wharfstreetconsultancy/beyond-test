f7af9e52678b2ebdf9c1d004efc6d1987fe6d15c
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-09-13 16:44:27 +0100

    Trying to figure out carousel probs.

--- webapp-marketplace/server.js
+++ webapp-marketplace/server.js
@@ -260,18 +260,29 @@ function formatProductViewHtml(product,callback) {
 				}
 */
 				// Construct item indicator for current image
+				itemIndicatorHtml += '<li data-target="#productCarousel" data-slide-to="'+(imageCounter++)+'"'+((image.isDefault) ? ' class="active"' : '')+'></li>';
+
+				// Construct image item reference for current image
+				itemImageHtml += '<div class="carousel-item'+((image.isDefault) ? ' active' : '')+'">';
+				itemImageHtml += '<img class="d-block img-fluid" src="'+image.location+'" width="175" alt="'+product.name+'">';
+				itemImageHtml += '</div>';
+
+/*
+				// Construct item indicator for current image
 				itemIndicatorHtml += '<li data-target=\'#productCarousel\' data-slide-to=\''+(imageCounter++)+'\''+((image.isDefault) ? ' class=\'active\'' : '')+'></li>';
 
 				// Construct image item reference for current image
 				itemImageHtml += '<div class=\'carousel-item'+((image.isDefault) ? ' active' : '')+'\'>';
-				itemImageHtml += '<img class=\'d-block img-fluid\' src=\''+image.location+'\' alt=\''+product.name+'\'>';
+				itemImageHtml += '<img class=\'d-block img-fluid\' src=\''+image.location+'\' width=\'175\' alt=\''+product.name+'\'>';
 				itemImageHtml += '</div>';
+*/
 			}
 			
 			// Load carousel template and replace item indicators and references
 			var carouselStream = fs.createReadStream(__dirname+'/carousel.html', 'utf8')
 				.pipe(replaceStream('{item.indicators}', itemIndicatorHtml))
 				.pipe(replaceStream('{item.images}', itemImageHtml));
+			carouselStream.on('open', function (data) {productImageCarouselHtml = '';});
 			carouselStream.on('data', function (data) {productImageCarouselHtml += data;});
 			carouselStream.on('end', function () {
 				console.log("Constructed carousel HTML: "+productImageCarouselHtml);
