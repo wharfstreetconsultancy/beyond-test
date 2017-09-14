c85a81569e74582629ac6fafa5c02c34a8eb1c2c
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-09-14 22:11:43 +0100

    Setting Location header.

--- webapp-marketplace/server.js
+++ webapp-marketplace/server.js
@@ -339,7 +339,7 @@ app.post('/cart/:id/item', function (req, res) {
         var id = '09876543';
 		// Return new cart item product list to caller
 		res.writeHead(201, {'Content-Type': 'application/json'});
-		res.writeHead({Location: 'https://'+restDomain+'/cart/item/'+id});
+		res.setHeader({Location: 'https://'+restDomain+'/cart/item/'+id});
 		res.write(JSON.stringify({productId: '111', quantity: 2, color: '333', size: '444', cost: '6.66'}));
 		res.end();
 });
