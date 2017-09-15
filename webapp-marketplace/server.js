6afac3bc3eb79f1ec6f7d9e4a26a9193020b8326
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-09-15 10:12:35 +0100

    Added cart size header.

--- webapp-marketplace/server.js
+++ webapp-marketplace/server.js
@@ -337,9 +337,11 @@ app.post('/cart/:id/item', function (req, res) {
         console.log( "Received request: POST /cart/"+req.params.id+"/item" );
 
         var id = '09876543';
+    	var timestamp = new Date().getTime().toString();
+        var cartSize = 0;
 		// Return new cart item product list to caller
-		res.writeHead(201, {'Content-Type': 'application/json', Location: 'https://'+restDomain+'/cart/item/'+id});
+		res.writeHead(201, {'Content-Type': 'application/json', Location: 'https://'+restDomain+'/cart/item/'+id, CartSize: cartSize++});
 //		res.setHeader({Location: 'https://'+restDomain+'/cart/item/'+id});
-		res.write(JSON.stringify({productId: '111', quantity: 2, color: '333', size: '444', cost: '6.66'}));
+		res.write(JSON.stringify({productId: '111', quantity: 2, color: '333', size: '444', cost: '6.66', lastUpdated: timestamp}));
 		res.end();
 });
