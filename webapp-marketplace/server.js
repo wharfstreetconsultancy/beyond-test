4725593a5a33bff1c514e8f23269e1ea7cf48d43
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-09-20 22:12:16 +0100

    Redirecting to passport.

--- webapp-marketplace/server.js
+++ webapp-marketplace/server.js
@@ -124,12 +124,12 @@ app.get('/', function (req, res) {
 	});
 });
 
-//passport.authenticate('cognito', {
-//successRedirect: '/',
-//failureRedirect: '/login'
-//}), 
+//
 // GET '/login' - Authenticate user
-app.post('/login', function (req, res) {
+app.post('/login', passport.authenticate('cognito', {
+		successRedirect: '/success.html',
+		failureRedirect: '/failure.html'
+	}), function (req, res) {
 	console.log("Username: "+req.body.username);
 	console.log("Password: "+req.body.password);
 	res.end();
