4f5ad1f77e83a3bbc8320426a6eb119186a774c2
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-09-23 00:29:19 +0100

    Accessing properties of the result directly.

--- webapp-marketplace/server.js
+++ webapp-marketplace/server.js
@@ -190,9 +190,9 @@ app.post('/signin', function (req, res) {
 	var cognitoUser = new AWS.CognitoIdentityServiceProvider.CognitoUser({Username: req.body.username, Pool: userPool});
 	cognitoUser.authenticateUser(authenticationDetails, {
 		onSuccess: function (result) {
-			console.log('ID Token: ' + result.getIdToken().getJwtToken());
-			console.log('Access Token: ' + result.getAccessToken().getJwtToken());
-			console.log('Refresh Token: ' + result.getRefreshToken().getJwtToken());
+			console.log('ID Token: ' + result.idToken.jwtToken);
+			console.log('Access Token: ' + result.accessToken.jwtToken);
+			console.log('Refresh Token: ' + result.refreshToken.jwtToken);
 		},
 		onFailure: function (err) {
 			console.log("!ERROR! - "+err);
