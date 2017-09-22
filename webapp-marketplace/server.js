9d4f8a2c4fc9ed6654a131c393433c116b3b2188
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-09-23 00:02:05 +0100

    Experimenting.

--- webapp-marketplace/server.js
+++ webapp-marketplace/server.js
@@ -13,9 +13,9 @@ var dddc = new AWS.DynamoDB.DocumentClient({apiVersion: '2012-08-10'});
 var s3 = new AWS.S3({apiVersion: '2006-03-01'});
 var cognitoIdentityServiceProvider = new AWS.CognitoIdentityServiceProvider({apiVersion: '2016-04-18'});
 //var cognitoUserPool = new AWS.CognitoIdentityServiceProvider.CognitoUserPool({apiVersion: '2016-04-18'});
-// var CognitoSDK = require('amazon-cognito-identity-js-node');
-// AWS.CognitoIdentityServiceProvider.CognitoUserPool = CognitoSDK.CognitoUserPool;
-// AWS.CognitoIdentityServiceProvider.CognitoUser = CognitoSDK.CognitoUser;
+var CognitoSDK = require('amazon-cognito-identity-js-node');
+AWS.CognitoIdentityServiceProvider.CognitoUserPool = CognitoSDK.CognitoUserPool;
+AWS.CognitoIdentityServiceProvider.CognitoUser = CognitoSDK.CognitoUser;
 // AWS.CognitoIdentityServiceProvider.CognitoUserAttribute = CognitoSDK.CognitoUserAttribute;
 // var userPool = new AWS.CognitoIdentityServiceProvider.CognitoUserPool({ UserPoolId : 'us-west-2_jnmkbOGZY', ClientId : 'm1f0r4q7uqgr9vd0qbqouspha'});
 
@@ -178,7 +178,7 @@ app.post('/signin', function (req, res) {
 	console.log("Username: "+req.body.username);
 	console.log("Password: "+req.body.password);
 
-	var userPool = cognitoIdentityServiceProvider.cognitoUserPool({
+	var userPool = new cognitoIdentityServiceProvider.CognitoUserPool({
 	    UserPoolId : 'us-west-2_jnmkbOGZY',
 	    ClientId : 'm1f0r4q7uqgr9vd0qbqouspha'
 	});
@@ -186,7 +186,7 @@ app.post('/signin', function (req, res) {
 		Username: req.body.username,
 		Password: req.body.password
 	});
-	var cognitoUser = cognitoIdentityServiceProvider.CognitoUser;
+	var cognitoUser = cognitoIdentityServiceProvider.CognitoUser({Username: req.body.username, Pool: userPool});
 	cognitoUser.authenticateUser(authenticationDetails, {
 		onSuccess: function (result) {
 			console.log('access token + ' + result.getAccessToken().getJwtToken());
