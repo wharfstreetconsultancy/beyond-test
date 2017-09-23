9eb72d2c16b5853ef9b2636e680b7a9f5c926933
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-09-23 15:24:33 +0100

    Using SDK for user sign-up.

--- webapp-marketplace/server.js
+++ webapp-marketplace/server.js
@@ -121,29 +121,42 @@ app.post('/signup', function (req, res) {
 	console.log("Password: "+req.body.password);
 	console.log("Phone number: "+req.body.phone_number);
 
-	var params = {
-				ClientId: 'm1f0r4q7uqgr9vd0qbqouspha',
-				Password: req.body.password,
-				Username: req.body.username,
+//	var params = {
+//				ClientId: 'm1f0r4q7uqgr9vd0qbqouspha',
+//				Password: req.body.password,
+//				Username: req.body.username,
 //				SecretHash: 'STRING_VALUE',
-				UserAttributes: [
-					new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute({Name: 'phone_number', Value: req.body.phone_number}),
-					new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute({Name: 'address', Value: 'dummy address'}),
-					new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute({Name: 'given_name', Value: 'dummy given name'}),
-					new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute({Name: 'family_name', Value: 'dummy family name'})
-				],
-				ValidationData: []
-			};
-	cognitoIdentityServiceProvider.signUp(params, function(err, data) {
-		if (err) {
-			console.log("!ERROR! "+err);
-		} else {
-			console.log(data);
+//				UserAttributes: [
+//					new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute({Name: 'phone_number', Value: req.body.phone_number}),
+//					new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute({Name: 'address', Value: 'dummy address'}),
+//					new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute({Name: 'given_name', Value: 'dummy given name'}),
+//					new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute({Name: 'family_name', Value: 'dummy family name'})
+//				],
+//				ValidationData: []
+//			};
 
-            // Return response to caller
-            res.writeHead(200, {'Content-Type': 'application/json'});
-            res.end();
-		}
+	userPool.signUp(
+		req.body.username,
+		req.body.password,
+		[
+			new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute({Name: 'phone_number', Value: req.body.phone_number}),
+			new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute({Name: 'address', Value: 'dummy address'}),
+			new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute({Name: 'given_name', Value: 'dummy given name'}),
+			new AWS.CognitoIdentityServiceProvider.CognitoUserAttribute({Name: 'family_name', Value: 'dummy family name'})
+		],
+		null,
+		function () {
+
+//	cognitoIdentityServiceProvider.signUp(params, function(err, data) {
+			if (err) {
+				console.log("!ERROR! - Failed to sign-up user: "+err);
+			} else {
+				console.log(data);
+	
+	            // Return response to caller
+	            res.writeHead(200, {'Content-Type': 'application/json'});
+	            res.end();
+			}
 	});
 });
 
