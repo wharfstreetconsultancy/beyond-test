2a82a1f3b75175bf26670a4450159a0d65353c35
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-09-23 10:56:16 +0100

    Stringify body of response.

--- webapp-marketplace/server.js
+++ webapp-marketplace/server.js
@@ -173,7 +173,7 @@ app.post('/signin', function (req, res) {
 			console.log('Login success.');
 			// Return response to caller
             res.writeHead(201, {'Content-Type': 'application/json'});
-            res.write({name: 'paul'});
+            res.write(JSON.stringify({name: 'paul'}));
             res.end();
 		}
 	});
