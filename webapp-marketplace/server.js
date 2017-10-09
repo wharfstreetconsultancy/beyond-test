7ff97c11fc3b4b235029d0b26017f0e240663703
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-10-09 23:02:45 +0100

    Testing string input.

--- webapp-marketplace/server.js
+++ webapp-marketplace/server.js
@@ -27,7 +27,7 @@ var userPool = new AWS.CognitoIdentityServiceProvider.CognitoUserPool({
 	ClientId: process.env.AUTH_CLIENT
 	// ClientId: 'm1f0r4q7uqgr9vd0qbqouspha'
 });
-console.log("PAYMENT_GATEWAY="+process.env.PAYMENT_GATEWAY);
+console.log("PAYMENT_GATEWAY="+JSON.stringify(process.env.PAYMENT_GATEWAY.split()));
 var gateway = braintree.connect({
 	accessToken: process.env.PAYMENT_GATEWAY
 //	accessToken: 'access_token$sandbox$vbv95xvqd975334w$3d4f9a155ac65d3340d295c3feeac65c'
