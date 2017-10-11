b76ea0ade27bcc74227f94ad76130e8d76b29f0b
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-10-11 22:23:59 +0100

    Separated out to individual sudo calls.

--- webapp-productmanagement/deploy.sh
+++ webapp-productmanagement/deploy.sh
@@ -1,4 +1,9 @@
 #!/bin/bash
 
-sudo -H -u root bash -c 'docker stop webapp_productmanagement-latest; docker rm webapp_productmanagement-latest; docker build -t suroor-fashions/webapp-productmanagement:latest .'
-sudo -H -u root bash -c 'docker run -p 181:8081 -p 1444:8444 -d -e ENVIRONMENT="$1" -e SESSION_SECRET=${2} -e AWS_ACCESS_KEY_ID=${3} -e AWS_SECRET_ACCESS_KEY=${4} -e AWS_REGION=us-west-2 -e PORT=1444 --name webapp_productmanagement-latest suroor-fashions/webapp-productmanagement:latest; docker logs webapp_productmanagement-latest --follow'
+# sudo -H -u root bash -c 'docker stop webapp_productmanagement-latest; docker rm webapp_productmanagement-latest; docker build -t suroor-fashions/webapp-productmanagement:latest .'
+# sudo -H -u root bash -c 'docker run -p 181:8081 -p 1444:8444 -d -e ENVIRONMENT="$1" -e SESSION_SECRET=${2} -e AWS_ACCESS_KEY_ID=${3} -e AWS_SECRET_ACCESS_KEY=${4} -e AWS_REGION=us-west-2 -e PORT=1444 --name webapp_productmanagement-latest suroor-fashions/webapp-productmanagement:latest; docker logs webapp_productmanagement-latest --follow'
+sudo docker stop webapp_productmanagement-latest
+sudo docker rm webapp_productmanagement-latest
+sudo docker build -t suroor-fashions/webapp-productmanagement:latest .
+sudo docker run -p 181:8081 -p 1444:8444 -d -e ENVIRONMENT=$1 -e SESSION_SECRET=$2 -e AWS_ACCESS_KEY_ID=$3 -e AWS_SECRET_ACCESS_KEY=$4 -e AWS_REGION=us-west-2 -e PORT=1444 --name webapp_productmanagement-latest suroor-fashions/webapp-productmanagement:latest
+sudo docker logs webapp_productmanagement-latest --follow
