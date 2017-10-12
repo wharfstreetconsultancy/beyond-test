eca37edb575c1e91c3581f11a3cad23e830bf111
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-10-12 20:07:40 +0100

    Commenting out the  page rendering within cart controller.

--- webapp-marketplace/assets/js/cart.js
+++ webapp-marketplace/assets/js/cart.js
@@ -2,6 +2,7 @@
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery.form/3.51/jquery.form.min.js"></script>
 <!-- script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script -->
 <script>
+/*
 function reloadCustomerCart() {
 
 	var localCart = JSON.parse(localStorage.getItem('cart'));
@@ -39,7 +40,7 @@ function reloadCustomerCart() {
 		}
 	}
 }
-
+*/
 $(document).ready(function() {
 
 	reloadCustomerCart(function () {
