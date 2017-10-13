165824b334911ac807fb6d917410ac0c3c0e0808
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-10-13 20:12:49 +0100

    Inspecting transaction objetc, even on failure.

--- webapp-marketplace/server.js
+++ webapp-marketplace/server.js
@@ -1087,6 +1087,7 @@ app.post('/transaction', function (req, res) {
 			console.log("Payment transaction returned an error ("+err.type+")");
 			console.log("Payment transaction returned an error ("+err.name+")");
 			console.log("Payment transaction returned an error ("+err.message+")");
+			console.log("Payment transaction returned an error ("+result.transaction+")");
 
 			// Return new cart item to caller
 			res.writeHead(500, {'Content-Type': 'application/json'});
