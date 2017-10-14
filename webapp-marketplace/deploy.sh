3d0f152fd7c56fc3d72b8eef8b2656f1efe9ae7b
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-10-14 02:25:34 +0100

    Typo fix.

--- webapp-marketplace/deploy.sh
+++ webapp-marketplace/deploy.sh
@@ -5,11 +5,8 @@ read oldVersion
 echo Start which version?:
 read newVersion
 
-echo oldVersion=$oldVersion
-echo newVersion=$newVersion
-
-# sudo docker stop webapp_marketplace-$oldVersion
-# sudo docker rm webapp_marketplace-$oldVersion
-# sudo docker build -t suroor-fashions/webapp-marketplace:$newVersion .
-# sudo docker run -p 180:8080 -p 1443:8443 -d -e ENVIRONMENT=$1 -e SESSION_SECRET=$2 -e AWS_ACCESS_KEY_ID=$3 -e AWS_SECRET_ACCESS_KEY=$4 -e AWS_REGION=us-west-2 -e SECURE_PORT=1443 -e REST_HOST=$5 -e REST_PORT=1444 -e AUTH_CLIENT=$6 --name webapp_marketplace-$newVersion suroor-fashions/webapp-marketplace:$newVersion
-# sudo docker logs webapp_marketplace-$newVersion --follow
+sudo docker stop webapp_marketplace-$oldVersion
+sudo docker rm webapp_marketplace-$oldVersion
+sudo docker build -t suroor-fashions/webapp-marketplace:$newVersion .
+sudo docker run -p 180:8080 -p 1443:8443 -d -e ENVIRONMENT=$1 -e SESSION_SECRET=$2 -e AWS_ACCESS_KEY_ID=$3 -e AWS_SECRET_ACCESS_KEY=$4 -e AWS_REGION=us-west-2 -e SECURE_PORT=1443 -e REST_HOST=$5 -e REST_PORT=1444 -e AUTH_CLIENT=$6 --name webapp_marketplace-$newVersion suroor-fashions/webapp-marketplace:$newVersion
+sudo docker logs webapp_marketplace-$newVersion --follow
--- webapp-productmanagement/deploy.sh
+++ webapp-productmanagement/deploy.sh
@@ -5,11 +5,8 @@ read oldVersion
 echo Start which version?:
 read newVersion
 
-echo oldVersion=$oldVersion
-echo newVersion=$newVersion
-
-# sudo docker stop webapp_productmanagement-$oldVersion
-# sudo docker rm webapp_productmanagement-$oldVersion
-# sudo docker build -t suroor-fashions/webapp-productmanagement:$newVersio .
-# sudo docker run -p 181:8081 -p 1444:8444 -d -e ENVIRONMENT=$1 -e SESSION_SECRET=$2 -e AWS_ACCESS_KEY_ID=$3 -e AWS_SECRET_ACCESS_KEY=$4 -e AWS_REGION=us-west-2 -e SECURE_PORT=1444 -e REST_HOST=$5 -e REST_PORT=1444 -e ALLOWED_ORIGIN_PORT=1444 --name webapp_productmanagement-$newVersio suroor-fashions/webapp-productmanagement:$newVersio
-# sudo docker logs webapp_productmanagement-$newVersio --follow
+sudo docker stop webapp_productmanagement-$oldVersion
+sudo docker rm webapp_productmanagement-$oldVersion
+sudo docker build -t suroor-fashions/webapp-productmanagement:$newVersio .
+sudo docker run -p 181:8081 -p 1444:8444 -d -e ENVIRONMENT=$1 -e SESSION_SECRET=$2 -e AWS_ACCESS_KEY_ID=$3 -e AWS_SECRET_ACCESS_KEY=$4 -e AWS_REGION=us-west-2 -e SECURE_PORT=1444 -e REST_HOST=$5 -e REST_PORT=1444 -e ALLOWED_ORIGIN_PORT=1444 --name webapp_productmanagement-$newVersio suroor-fashions/webapp-productmanagement:$newVersio
+sudo docker logs webapp_productmanagement-$newVersio --follow
