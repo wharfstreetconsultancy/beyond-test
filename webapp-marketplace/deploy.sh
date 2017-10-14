14b3e724705c3e0fa55024d0c5cdb1e636e50326
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-10-14 02:37:20 +0100

    Typo fix.

--- webapp-marketplace/deploy.sh
+++ webapp-marketplace/deploy.sh
@@ -8,11 +8,11 @@ echo target environment?:
 read environment
 
 if [ "$env" == "sandbox" ]; then
-	export port = '1'
+	export desired_port = '1'
 fi
 
 sudo docker stop webapp_marketplace-$oldVersion
 sudo docker rm webapp_marketplace-$oldVersion
 sudo docker build -t suroor-fashions/webapp-marketplace:$newVersion .
-sudo docker run -p $port80:8080 -p $port443:8443 -d -e ENVIRONMENT=$1 -e SESSION_SECRET=$2 -e AWS_ACCESS_KEY_ID=$3 -e AWS_SECRET_ACCESS_KEY=$4 -e AWS_REGION=us-west-2 -e SECURE_PORT=$port443 -e REST_HOST=$5 -e REST_PORT=$port444 -e AUTH_CLIENT=$6 --name webapp_marketplace-$newVersion suroor-fashions/webapp-marketplace:$newVersion
+sudo docker run -p $desired_port80:8080 -p $desired_port443:8443 -d -e ENVIRONMENT=$1 -e SESSION_SECRET=$2 -e AWS_ACCESS_KEY_ID=$3 -e AWS_SECRET_ACCESS_KEY=$4 -e AWS_REGION=us-west-2 -e SECURE_PORT=$desired_port443 -e REST_HOST=$5 -e REST_PORT=$desired_port444 -e AUTH_CLIENT=$6 --name webapp_marketplace-$newVersion suroor-fashions/webapp-marketplace:$newVersion
 sudo docker logs webapp_marketplace-$newVersion --follow
--- webapp-productmanagement/deploy.sh
+++ webapp-productmanagement/deploy.sh
@@ -5,8 +5,12 @@ read oldVersion
 echo Start which version?:
 read newVersion
 
+if [ "$env" == "sandbox" ]; then
+	export desired_port = '1'
+fi
+
 sudo docker stop webapp_productmanagement-$oldVersion
 sudo docker rm webapp_productmanagement-$oldVersion
 sudo docker build -t suroor-fashions/webapp-productmanagement:$newVersio .
-sudo docker run -p 181:8081 -p 1444:8444 -d -e ENVIRONMENT=$1 -e SESSION_SECRET=$2 -e AWS_ACCESS_KEY_ID=$3 -e AWS_SECRET_ACCESS_KEY=$4 -e AWS_REGION=us-west-2 -e SECURE_PORT=1444 -e REST_HOST=$5 -e REST_PORT=1444 -e ALLOWED_ORIGIN_PORT=1444 --name webapp_productmanagement-$newVersio suroor-fashions/webapp-productmanagement:$newVersio
+sudo docker run -p $desired_port81:8081 -p $desired_port444:8444 -d -e ENVIRONMENT=$1 -e SESSION_SECRET=$2 -e AWS_ACCESS_KEY_ID=$3 -e AWS_SECRET_ACCESS_KEY=$4 -e AWS_REGION=us-west-2 -e SECURE_PORT=$desired_port444 -e REST_HOST=$5 -e REST_PORT=$desired_port444 -e ALLOWED_ORIGIN_PORT=$desired_port444 --name webapp_productmanagement-$newVersio suroor-fashions/webapp-productmanagement:$newVersio
 sudo docker logs webapp_productmanagement-$newVersio --follow
