b95b6e2f4a9c917aa7687313a6019638c8f004bd
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-10-14 12:48:41 +0100

    Re skin.

---
+++ webapp-marketplace/assets/js/body_footer.js
@@ -0,0 +1,14 @@
+<footer>
+	<div class="container-fluid">
+		<div class="footer">
+			<p class="copyright">&copy 2017 Suroor Fasions | All Rights Reserved</p>
+			<p class="links"><a href="#">Terms of use </a></p>
+			<span class="links">|</span>
+			<p class="links"><a href="#">Privacy Policy </a></p>
+			<span class="links">|</span>
+			<p class="links"><a href="#">Sales & Return Policy</a></p>
+		</div>
+	</div>
+</footer>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js" integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous"></script>
