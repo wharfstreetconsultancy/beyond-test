4ed4c7100fbe8909391e94c3be6280d259596044
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-10-14 02:24:10 +0100

    Prompting for old and new versions when running script.

--- webapp-productmanagement/deploy.sh
+++ webapp-productmanagement/deploy.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
 
-sudo docker stop webapp_productmanagement-latest
-sudo docker rm webapp_productmanagement-latest
-sudo docker build -t suroor-fashions/webapp-productmanagement:latest .
-sudo docker run -p 181:8081 -p 1444:8444 -d -e ENVIRONMENT=$1 -e SESSION_SECRET=$2 -e AWS_ACCESS_KEY_ID=$3 -e AWS_SECRET_ACCESS_KEY=$4 -e AWS_REGION=us-west-2 -e SECURE_PORT=1444 -e REST_HOST=$5 -e REST_PORT=1444 -e ALLOWED_ORIGIN_PORT=1444 --name webapp_productmanagement-latest suroor-fashions/webapp-productmanagement:latest
-sudo docker logs webapp_productmanagement-latest --follow
+echo Stop which version?:
+read oldVersion
+echo Start which version?:
+read newVersion
+
+echo oldVersion=$oldVersion
+echo newVersion=$newVersion
+
+# sudo docker stop webapp_productmanagement-$oldVersion
+# sudo docker rm webapp_productmanagement-$oldVersion
+# sudo docker build -t suroor-fashions/webapp-productmanagement:$newVersio .
+# sudo docker run -p 181:8081 -p 1444:8444 -d -e ENVIRONMENT=$1 -e SESSION_SECRET=$2 -e AWS_ACCESS_KEY_ID=$3 -e AWS_SECRET_ACCESS_KEY=$4 -e AWS_REGION=us-west-2 -e SECURE_PORT=1444 -e REST_HOST=$5 -e REST_PORT=1444 -e ALLOWED_ORIGIN_PORT=1444 --name webapp_productmanagement-$newVersio suroor-fashions/webapp-productmanagement:$newVersio
+# sudo docker logs webapp_productmanagement-$newVersio --follow
