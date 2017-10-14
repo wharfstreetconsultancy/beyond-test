5ea933c2c81e396cb7b833bd1e3ff47d2d7023d8
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-10-14 15:31:42 +0100

    Typo fix.

--- webapp-marketplace/server.js
+++ webapp-marketplace/server.js
@@ -368,7 +368,7 @@ function formatProductsCarouselsHtml(productsList,callback) {
 					for(var image of product.images) {
 						if(image.isDefault) {
 							console.log("Product ID: "+product.id);
-							currentBuffer += '<a href=\'/product?id='+product.id+'\'><img src=\''+image.location+'\' alt=\''+product.name+'\'></a>';
+							currentBuffer += '<a href=\'/product?id='+product.id+'\'><img src=\''+image.location+'\' width=\'150\' alt=\''+product.name+'\'></a>';
 						}
 					}
 				}
@@ -477,7 +477,7 @@ function formatProductViewHtml(product,callback) {
 
 				// Construct image item reference for current image
 				itemImageHtml += '<div class=\'item'+((image.isDefault) ? ' active' : '')+'\'>';
-				itemImageHtml += '<img src=\''+image.location+'\' alt=\''+product.name+'\'>';
+				itemImageHtml += '<img src=\''+image.location+'\' width=\'150\' alt=\''+product.name+'\'>';
 				itemImageHtml += '</div>';
 
 			}
