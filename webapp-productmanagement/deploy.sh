a881f5a73a62a80835589fbdedd56efdd525a236
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-10-14 02:45:12 +0100

    Typo fix.

--- webapp-productmanagement/deploy.sh
+++ webapp-productmanagement/deploy.sh
@@ -8,11 +8,13 @@ echo target environment?:
 read environment
 
 if [ "$environment" == "sandbox" ]; then
-	export desired_port = '1'
+	export desired_port='1'
 fi
+
+echo Test Port = $desired_port'443'
 
 sudo docker stop webapp_productmanagement-$oldVersion
 sudo docker rm webapp_productmanagement-$oldVersion
 sudo docker build -t suroor-fashions/webapp-productmanagement:$newVersio .
-sudo docker run -p $desired_port81:8081 -p $desired_port444:8444 -d -e ENVIRONMENT=$1 -e SESSION_SECRET=$2 -e AWS_ACCESS_KEY_ID=$3 -e AWS_SECRET_ACCESS_KEY=$4 -e AWS_REGION=us-west-2 -e SECURE_PORT=$desired_port444 -e REST_HOST=$5 -e REST_PORT=$desired_port444 -e ALLOWED_ORIGIN_PORT=$desired_port444 --name webapp_productmanagement-$newVersio suroor-fashions/webapp-productmanagement:$newVersio
+sudo docker run -p $desired_port'81:8081' -p $desired_port'444:8444' -d -e ENVIRONMENT=$1 -e SESSION_SECRET=$2 -e AWS_ACCESS_KEY_ID=$3 -e AWS_SECRET_ACCESS_KEY=$4 -e AWS_REGION=us-west-2 -e SECURE_PORT=$desired_port444 -e REST_HOST=$5 -e REST_PORT=$desired_port'444' -e ALLOWED_ORIGIN_PORT=$desired_port'444' --name webapp_productmanagement-$newVersio suroor-fashions/webapp-productmanagement:$newVersio
 sudo docker logs webapp_productmanagement-$newVersio --follow
