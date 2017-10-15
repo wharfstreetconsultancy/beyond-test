961b36a23feba1af907f179beda31eef77ccb222
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-10-15 14:32:28 +0100

    Experiment.

--- webapp-marketplace/assets/js/body_header.js
+++ webapp-marketplace/assets/js/body_header.js
@@ -8,16 +8,6 @@
     width: 100%; /* Full width */
 }
 
-/* Links inside the navbar */
-.navbar a {
-    float: left;
-    display: block;
-    color: #f2f2f2;
-    text-align: center;
-    padding: 14px 16px;
-    text-decoration: none;
-}
-
 /* Main content */
 .main {
     margin-top: 30px; /* Add a top margin to avoid content overlay */
