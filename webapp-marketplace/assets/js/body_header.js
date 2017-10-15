c7cc55a44c2c2641494eabca02393d70577896c7
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-10-15 14:44:03 +0100

    Trying to fix both banner items.

--- webapp-marketplace/assets/js/body_header.js
+++ webapp-marketplace/assets/js/body_header.js
@@ -1,7 +1,7 @@
 <style>
 /* The navigation bar */
 //.navbar {
-#suroor_header {
+#suroor_banner, #suroor_navbar {
 //    overflow: hidden;
     background-color: #000;
     position: fixed; /* Set the navbar to fixed position */
@@ -14,50 +14,48 @@
     margin-top: 30px; /* Add a top margin to avoid content overlay */
 }
 </style>
-<span id="suroor_header">
-	<div class="container-fluid">
-		<div class="row">
-			<div class="col-sm-1 header">
-				<div class="col-xs-1 col-md-1 col-lg-1 text-center">
-					<a href="/"><img src="img/SVGlogo.svg" class="img" alt="Suroor Fashions - Marketplace"></a>
-				</div>
+<div id="suroor_banner" class="container-fluid">
+	<div class="row">
+		<div class="col-sm-1 header">
+			<div class="col-xs-1 col-md-1 col-lg-1 text-center">
+				<a href="/"><img src="img/SVGlogo.svg" class="img" alt="Suroor Fashions - Marketplace"></a>
 			</div>
 		</div>
 	</div>
-	<nav class="navbar navbar-default">
-		<div class="container-fluid nav-con">
-			<!--Brand and toggle get grouped for better mobile display-->
-			<div class="navbar-header">
-				<div class="col-sm-1 header">
-					<button type="button" class="navbar-toggle x collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-						<span class="sr-only">Toggle navigation</span>
-						<span class="icon-bar"></span>
-						<span class="icon-bar second-bar"></span>
-					</button>
-				</div>
-			</div>
-			<!--Collect the nav links, forms, and other content for toggling-->
-			<div class="collapse navbar-collapse cool-nav" id="bs-example-navbar-collapse-1">
-				<ul class="myNav nav navbar-nav">
-					<li><a href="/shop">Shop<span class="sr-only">(current)</span></a></li>
-					<li><a href="/about">About Us<span class="sr-only">(current)</span></a></li>
-					<li><a href="/contact">Contact</a></li>
-					<!-- li><a id="user_nav" class="dropdown-toggle fa fa-user" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></a>
-					<div class="dropdown-menu pull-right" aria-labelledby="user_nav"><form id="identity" class="form-inline" method="post"></form></div></li -->
-					<!-- li><a id="cart_preview_nav" class="dropdown-toggle fa fa-shopping-cart" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> My Cart</a>
-					<div class="dropdown-menu pull-right" aria-labelledby="cart_preview_nav"><form id="cart_preview" action="/checkout_confirmation" method="post" class="form-inline"></form></div></li -->
-					<li>
-						<a id="cart_link" class="fa fa-shopping-cart" href="#"> My Cart</a>
-						<form id="cart_manager" action="/cart" method="post"/>
-					</li>
-					<li>
-						<a id="cust_link" class="dropdown-toggle fa fa-user" href="#"/>
-					</li>
-				</ul>
+</div>
+<nav id="suroor_navbar" class="navbar navbar-default">
+	<div class="container-fluid nav-con">
+		<!--Brand and toggle get grouped for better mobile display-->
+		<div class="navbar-header">
+			<div class="col-sm-1 header">
+				<button type="button" class="navbar-toggle x collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+					<span class="sr-only">Toggle navigation</span>
+					<span class="icon-bar"></span>
+					<span class="icon-bar second-bar"></span>
+				</button>
 			</div>
 		</div>
-	</nav>
-</span>
+		<!--Collect the nav links, forms, and other content for toggling-->
+		<div class="collapse navbar-collapse cool-nav" id="bs-example-navbar-collapse-1">
+			<ul class="myNav nav navbar-nav">
+				<li><a href="/shop">Shop<span class="sr-only">(current)</span></a></li>
+				<li><a href="/about">About Us<span class="sr-only">(current)</span></a></li>
+				<li><a href="/contact">Contact</a></li>
+				<!-- li><a id="user_nav" class="dropdown-toggle fa fa-user" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"></a>
+				<div class="dropdown-menu pull-right" aria-labelledby="user_nav"><form id="identity" class="form-inline" method="post"></form></div></li -->
+				<!-- li><a id="cart_preview_nav" class="dropdown-toggle fa fa-shopping-cart" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false"> My Cart</a>
+				<div class="dropdown-menu pull-right" aria-labelledby="cart_preview_nav"><form id="cart_preview" action="/checkout_confirmation" method="post" class="form-inline"></form></div></li -->
+				<li>
+					<a id="cart_link" class="fa fa-shopping-cart" href="#"> My Cart</a>
+					<form id="cart_manager" action="/cart" method="post"/>
+				</li>
+				<li>
+					<a id="cust_link" class="dropdown-toggle fa fa-user" href="#"/>
+				</li>
+			</ul>
+		</div>
+	</div>
+</nav>
 <script>
 $(document).ready(function() {
 	var customer = sessionStorage.getItem('customer');
