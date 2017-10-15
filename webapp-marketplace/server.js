40cc181fc57b0e6d9cee5f7c0ac457612720f1b9
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-10-15 12:34:11 +0100

    Home page carousel.

--- webapp-marketplace/server.js
+++ webapp-marketplace/server.js
@@ -124,9 +124,9 @@ app.get('/', function (req, res) {
             // Add dynamic elements to response page
             fs.createReadStream(__dirname+'/index.html')
 				.pipe(replaceStream('{error.message}', '&nbsp;'))
-//				.pipe(replaceStream('{products.list}', JSON.stringify(productsList)))
-				.pipe(replaceStream('{showcase.clothing.carousel}', productsListClothingHtml))
-				.pipe(replaceStream('{showcase.jewellery.carousel}', productsListJewelleryHtml))
+				.pipe(replaceStream('{products.list}', JSON.stringify(productsList)))
+//				.pipe(replaceStream('{showcase.clothing.carousel}', productsListClothingHtml))
+//				.pipe(replaceStream('{showcase.jewellery.carousel}', productsListJewelleryHtml))
 				.pipe(res);
         });
 	});
