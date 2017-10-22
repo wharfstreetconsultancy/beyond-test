429036a8870d22a42fa2617eb5f80c733b88b7c7
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-10-22 22:26:17 +0100

    Typo fix.

--- webapp-marketplace/server.js
+++ webapp-marketplace/server.js
@@ -1201,7 +1201,7 @@ app.post('/create-payment', function (req, res) {
 									console.log("Got payment response: "+JSON.stringify(paymentResponse));
 									console.log("Got payment body: "+paymentBody);
 
-									var response = {id: JSON.parse(paymentBody).id}
+									var response = {paymentID: JSON.parse(paymentBody).id}
 									console.log("Returning to caller: "+JSON.stringify(response));
 									// Return error to caller
 									res.writeHead(201, {'Content-Type': 'application/json'});
