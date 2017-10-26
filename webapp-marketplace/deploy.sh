60ea94d1fb6f947e2477d37a4fd9370c879ffb1a
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-10-26 19:26:44 +0100

    Specifying profile on command line.

--- webapp-marketplace/deploy.sh
+++ webapp-marketplace/deploy.sh
@@ -5,8 +5,8 @@ read oldVersion
 echo Start which version?:
 read newVersion
 
-SRV_ENV=$(aws s3 cp s3://suroor.fashions.config/marketplace_server.$1.env.properties - --profile suroor-fashions_webapp-productmanagement)
-PGW_ENV=$(aws s3 cp s3://suroor.fashions.config/payment_gateway.$2.env.properties - --profile suroor-fashions_webapp-productmanagement)
+SRV_ENV=$(aws s3 cp s3://suroor.fashions.config/marketplace_server.$1.env.properties - --profile $3)
+PGW_ENV=$(aws s3 cp s3://suroor.fashions.config/payment_gateway.$2.env.properties - --profile $3)
 
 SESSION_SECRET=$(jq -r '.sessionSecret' <<< "${SRV_ENV}")
 REGION=$(jq -r '.region' <<< "${SRV_ENV}")
@@ -35,5 +35,6 @@ echo PGW_HOST=$PGW_HOST
 sudo docker stop webapp_marketplace-$oldVersion
 sudo docker rm webapp_marketplace-$oldVersion
 sudo docker build -t suroor-fashions/webapp-marketplace:$newVersion .
-sudo docker run -p $NON_SECURE_PORT:8080 -p $SECURE_PORT:8443 -d -e ENVIRONMENT=$1 -e SESSION_SECRET=$SESSION_SECRET -e AWS_ACCESS_KEY_ID=$3 -e AWS_SECRET_ACCESS_KEY=$4 -e AWS_REGION=$REGION -e SECURE_PORT=$SECURE_PORT -e REST_HOST=$AGW_HOST -e REST_PORT=$AGW_PORT -e AUTH_CLIENT=$USER_POOL_CLIENT -e PGW_HOST=$PGW_HOST -e PGW_CLIENT=$PGW_CLIENT -e PGW_SECRET=$PGW_SECRET --name webapp_marketplace-$newVersion suroor-fashions/webapp-marketplace:$newVersion
+#sudo docker run -p $NON_SECURE_PORT:8080 -p $SECURE_PORT:8443 -d -e ENVIRONMENT=$1 -e SESSION_SECRET=$SESSION_SECRET -e AWS_ACCESS_KEY_ID=$3 -e AWS_SECRET_ACCESS_KEY=$4 -e AWS_REGION=$REGION -e SECURE_PORT=$SECURE_PORT -e REST_HOST=$AGW_HOST -e REST_PORT=$AGW_PORT -e AUTH_CLIENT=$USER_POOL_CLIENT -e PGW_HOST=$PGW_HOST -e PGW_CLIENT=$PGW_CLIENT -e PGW_SECRET=$PGW_SECRET --name webapp_marketplace-$newVersion suroor-fashions/webapp-marketplace:$newVersion
+sudo docker run -p $NON_SECURE_PORT:8080 -p $SECURE_PORT:8443 -d -e ENVIRONMENT=$1 -e SESSION_SECRET=$SESSION_SECRET -e AWS_PROFILE=$3 -e AWS_REGION=$REGION -e SECURE_PORT=$SECURE_PORT -e REST_HOST=$AGW_HOST -e REST_PORT=$AGW_PORT -e AUTH_CLIENT=$USER_POOL_CLIENT -e PGW_HOST=$PGW_HOST -e PGW_CLIENT=$PGW_CLIENT -e PGW_SECRET=$PGW_SECRET --name webapp_marketplace-$newVersion suroor-fashions/webapp-marketplace:$newVersion
 sudo docker logs webapp_marketplace-$newVersion --follow
