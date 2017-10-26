88da6ef3f6ebc4fd94db4de464b6c5e914fd8cf9
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-10-26 22:42:32 +0100

    Importing profile and keys.

--- webapp-marketplace/deploy.sh
+++ webapp-marketplace/deploy.sh
@@ -5,13 +5,16 @@ read oldVersion
 echo Start which version?:
 read newVersion
 
-SRV_ENV=$(AWS_ACCESS_KEY_ID=$3; AWS_SECRET_ACCESS_KEY=$4; echo AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID; echo AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY; aws s3 cp s3://suroor.fashions.config/marketplace_server.$1.env.properties -)
-PGW_ENV=$(AWS_ACCESS_KEY_ID=$3; AWS_SECRET_ACCESS_KEY=$4; echo AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID; echo AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY; aws s3 cp s3://suroor.fashions.config/payment_gateway.$2.env.properties -)
-#SRV_ENV=$(aws s3 cp s3://suroor.fashions.config/marketplace_server.$1.env.properties - --profile $3)
-#PGW_ENV=$(aws s3 cp s3://suroor.fashions.config/payment_gateway.$2.env.properties - --profile $3)
+AWS_PROFILE=$3
+echo AWS_PROFILE=$AWS_PROFILE
+
+SRV_ENV=$(aws s3 cp s3://suroor.fashions.config/marketplace_server.$1.env.properties - --profile $AWS_PROFILE)
+PGW_ENV=$(aws s3 cp s3://suroor.fashions.config/payment_gateway.$2.env.properties - --profile $AWS_PROFILE)
 
 SESSION_SECRET=$(jq -r '.sessionSecret' <<< "${SRV_ENV}")
-REGION=$(jq -r '.region' <<< "${SRV_ENV}")
+AWS_ACCESS_KEY_ID=$4
+AWS_SECRET_ACCESS_KEY=$5
+AWS_REGION=$(jq -r '.region' <<< "${SRV_ENV}")
 USER_POOL_CLIENT=$(jq -r '.userPoolClient' <<< "${SRV_ENV}")
 NON_SECURE_PORT=$(jq -r '.nonSecurePort' <<< "${SRV_ENV}")
 SECURE_PORT=$(jq -r '.securePort' <<< "${SRV_ENV}")
@@ -23,7 +26,10 @@ PGW_SECRET=$(jq -r '.secret' <<< "${PGW_ENV}")
 PGW_HOST=$(jq -r '.host' <<< "${PGW_ENV}")
 
 echo SESSION_SECRET=$SESSION_SECRET
-echo REGION=$REGION
+echo AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
+echo AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
+echo AWS_REGION=$AWS_REGION
+echo AWS_REGION=$AWS_REGION
 echo USER_POOL_CLIENT=$USER_POOL_CLIENT
 echo NON_SECURE_PORT=$NON_SECURE_PORT
 echo SECURE_PORT=$SECURE_PORT
@@ -37,6 +43,6 @@ echo PGW_HOST=$PGW_HOST
 sudo docker stop webapp_marketplace-$oldVersion
 sudo docker rm webapp_marketplace-$oldVersion
 sudo docker build -t suroor-fashions/webapp-marketplace:$newVersion .
-sudo docker run -p $NON_SECURE_PORT:8080 -p $SECURE_PORT:8443 -d -e SRV_ENV=$1 -e PGW_ENV=$2 -e SESSION_SECRET=$SESSION_SECRET -e AWS_ACCESS_KEY_ID=$3 -e AWS_SECRET_ACCESS_KEY=$4 -e AWS_REGION=$REGION -e SECURE_PORT=$SECURE_PORT -e REST_HOST=$AGW_HOST -e REST_PORT=$AGW_PORT -e AUTH_CLIENT=$USER_POOL_CLIENT -e PGW_HOST=$PGW_HOST -e PGW_CLIENT=$PGW_CLIENT -e PGW_SECRET=$PGW_SECRET --name webapp_marketplace-$newVersion suroor-fashions/webapp-marketplace:$newVersion
-#sudo docker run -p $NON_SECURE_PORT:8080 -p $SECURE_PORT:8443 -d -e SRV_ENV=$1 -e PGW_ENV=$2 -e SESSION_SECRET=$SESSION_SECRET -e AWS_PROFILE=$3 -e AWS_REGION=$REGION -e SECURE_PORT=$SECURE_PORT -e REST_HOST=$AGW_HOST -e REST_PORT=$AGW_PORT -e AUTH_CLIENT=$USER_POOL_CLIENT -e PGW_HOST=$PGW_HOST -e PGW_CLIENT=$PGW_CLIENT -e PGW_SECRET=$PGW_SECRET --name webapp_marketplace-$newVersion suroor-fashions/webapp-marketplace:$newVersion
+sudo docker run -p $NON_SECURE_PORT:8080 -p $SECURE_PORT:8443 -d -e SRV_ENV=$1 -e PGW_ENV=$2 -e SESSION_SECRET=$SESSION_SECRET -e AWS_ACCESS_KEY_ID=$AWS_SECRET_ACCESS_KEY -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_REGION=$AWS_REGION -e SECURE_PORT=$SECURE_PORT -e REST_HOST=$AGW_HOST -e REST_PORT=$AGW_PORT -e AUTH_CLIENT=$USER_POOL_CLIENT -e PGW_HOST=$PGW_HOST -e PGW_CLIENT=$PGW_CLIENT -e PGW_SECRET=$PGW_SECRET --name webapp_marketplace-$newVersion suroor-fashions/webapp-marketplace:$newVersion
+#sudo docker run -p $NON_SECURE_PORT:8080 -p $SECURE_PORT:8443 -d -e SRV_ENV=$1 -e PGW_ENV=$2 -e SESSION_SECRET=$SESSION_SECRET -e AWS_PROFILE=$3 -e AWS_REGION=$AWS_REGION -e SECURE_PORT=$SECURE_PORT -e REST_HOST=$AGW_HOST -e REST_PORT=$AGW_PORT -e AUTH_CLIENT=$USER_POOL_CLIENT -e PGW_HOST=$PGW_HOST -e PGW_CLIENT=$PGW_CLIENT -e PGW_SECRET=$PGW_SECRET --name webapp_marketplace-$newVersion suroor-fashions/webapp-marketplace:$newVersion
 sudo docker logs webapp_marketplace-$newVersion --follow
