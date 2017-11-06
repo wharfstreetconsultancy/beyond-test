129d0bcb8dabdc460ac8906fda041181ce47f4ab
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-11-06 20:51:54 +0000

    Experiment.

--- webapp-marketplace/assets/js/body_header.js
+++ webapp-marketplace/assets/js/body_header.js
@@ -1,7 +1,7 @@
 <style>
 body { padding-top: 70px; }
 </style>
-<nav id="suroor_navbar" class="navbar navbar-default navbar-fixed-top">
+<nav class="navbar navbar-default navbar-fixed-top">
 	<div class="container-fluid">
 		<div class="navbar-header">
 			<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
