16447f602ff0f76f2de8121b7c31997c51f1702e
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2017-11-18 13:54:12 +0000

    Trying without navbar-brand class.

--- webapp-marketplace/assets/js/body_header.js
+++ webapp-marketplace/assets/js/body_header.js
@@ -8,7 +8,7 @@
 	      <span class="icon-bar"></span>
 	      <span class="icon-bar"></span>
 	    </button>
-	    <a class="navbar-brand" href="/"><img src="img/SVGlogo.svg" alt="Suroor Fashions - Marketplace"/></a>
+	    <a href="/"><img src="img/SVGlogo.svg" alt="Suroor Fashions - Marketplace"/></a>
 	  </div>
 	
 	  <!-- Collect the nav links, forms, and other content for toggling -->
