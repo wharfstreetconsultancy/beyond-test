7bee96d3e7e86323a2d5f3a2ad228001b77752f4
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2018-01-06 14:28:56 +0000

    Fixed incorrect signin page management on navbar.

--- webapp-marketplace/assets/js/header.js
+++ webapp-marketplace/assets/js/header.js
@@ -1,42 +1,39 @@
 <nav class="navbar navbar-default navbar-fixed-top">
 	<div class="container-fluid">
-	  <!-- Brand and toggle get grouped for better mobile display -->
-	  <div class="navbar-header">
-	    <button type="button" class="navbar-toggle collapsed pull-right" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-	      <span class="sr-only">Toggle navigation</span>
-	      <span class="icon-bar"></span>
-	      <span class="icon-bar"></span>
-	      <span class="icon-bar"></span>
-	    </button>
-	    <!-- a style="padding: 0px" class="navbar-brand" href="/"><img style="max-height: 50px; padding-left: 10px" src="img/SVGlogo.png" alt="Suroor Fashions - Marketplace"/></a -->
-	    <a style="padding: 0px 10px" class="navbar-brand" href="/"><img style="max-height: 50px; margin: auto" src="img/SVGlogo.png" alt="Suroor Fashions - Marketplace"/></a>
-	  </div>
+		<div class="navbar-header">
+			<button type="button" class="navbar-toggle collapsed pull-right" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+				<span class="sr-only">Toggle navigation</span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+				<span class="icon-bar"></span>
+			</button>
+			<a style="padding: 0px 10px" class="navbar-brand" href="/"><img style="max-height: 50px; margin: auto" src="img/SVGlogo.png" alt="Suroor Fashions - Marketplace"/></a>
+		</div>
 	
-	  <!-- Collect the nav links, forms, and other content for toggling -->
-	  <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-	    <ul class="nav navbar-nav navbar-right">
-	    	<li class="active">
-	  			<a id="showcase_link"  href="#" class="fa fa-star"> Featured</a>
-	  		</li>
-			<li>
-				<a id="shop_link" href="#" class="fa fa-tags"> Shop</a>
-			</li>
-			<li>
-				<a href="/about" class="fa fa-book"> About Us</a>
-			</li>
-			<li>
-				<a href="/contact" class="fa fa-phone"> Contact</a>
-			</li>
-			<li>
-				<a id="cart_link" href="#" class="fa fa-shopping-cart"> My Cart</a>
-				<form id="cart_manager" action="/cart" method="post"/>
-			</li>
-			<li>
-				<a id="cust_link" href="#" class="dropdown-toggle fa fa-user"/>
-			</li>
-	    </ul>
-	  </div><!-- /.navbar-collapse -->
-	</div><!-- /.container-fluid -->
+		<div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+			<ul class="nav navbar-nav navbar-right">
+		    	<li class="active">
+		  			<a id="showcase_link"  href="#" class="fa fa-star"> Featured</a>
+		  		</li>
+				<li>
+					<a id="shop_link" href="#" class="fa fa-tags"> Shop</a>
+				</li>
+				<li>
+					<a href="/about" class="fa fa-book"> About Us</a>
+				</li>
+				<li>
+					<a href="/contact" class="fa fa-phone"> Contact</a>
+				</li>
+				<li>
+					<a id="cart_link" href="#" class="fa fa-shopping-cart"> My Cart</a>
+					<form id="cart_manager" action="/cart" method="post"/>
+				</li>
+				<li>
+					<a id="cust_link" href="#" class="dropdown-toggle fa fa-user"/>
+				</li>
+			</ul>
+	    </div>
+	</div>
 </nav>
 <script>
 $(document).ready(function() {
@@ -102,7 +99,7 @@ $(document).ready(function() {
 			});
 		} else {
 
-			document.location.href = '/signin';
+			$("#body_main").load("js/signin.js");
 		}
  		return false;
 	});
