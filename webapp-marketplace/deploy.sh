84a14c51e956c5649d2585d2c018dec55264fd0d
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2018-03-26 22:53:03 +0100

    Created specific build script for marketplace now. To be invoked from the jenkins 'deploy' pipeline.

--- webapp-marketplace/deploy.sh
+++ webapp-marketplace/deploy.sh
@@ -1,18 +1,8 @@
 #!/bin/bash
 
-echo Stop which version?:
-read oldVersion
-echo Start which version?:
-read newVersion
+SRV_ENV=$(aws s3 cp s3://suroor.fashions.config/marketplace_server.sandbox.env.properties -)
+PGW_ENV=$(aws s3 cp s3://suroor.fashions.config/payment_gateway.sandbox.env.properties -)
 
-AWS_PROFILE=$3
-echo AWS_PROFILE=$AWS_PROFILE
-
-SRV_ENV=$(aws s3 cp s3://suroor.fashions.config/marketplace_server.$1.env.properties - --profile $AWS_PROFILE)
-PGW_ENV=$(aws s3 cp s3://suroor.fashions.config/payment_gateway.$2.env.properties - --profile $AWS_PROFILE)
-
-AWS_ACCESS_KEY_ID=$4
-AWS_SECRET_ACCESS_KEY=$5
 SESSION_SECRET=$(jq -r '.sessionSecret' <<< "${SRV_ENV}")
 AWS_REGION=$(jq -r '.region' <<< "${SRV_ENV}")
 USER_POOL_CLIENT=$(jq -r '.userPoolClient' <<< "${SRV_ENV}")
@@ -25,22 +15,5 @@ PGW_CLIENT=$(jq -r '.client' <<< "${PGW_ENV}")
 PGW_SECRET=$(jq -r '.secret' <<< "${PGW_ENV}")
 PGW_HOST=$(jq -r '.host' <<< "${PGW_ENV}")
 
-echo SESSION_SECRET=$SESSION_SECRET
-echo AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID
-echo AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY
-echo AWS_REGION=$AWS_REGION
-echo USER_POOL_CLIENT=$USER_POOL_CLIENT
-echo NON_SECURE_PORT=$NON_SECURE_PORT
-echo SECURE_PORT=$SECURE_PORT
-echo AGW_HOST=$AGW_HOST
-echo AGW_PORT=$AGW_PORT
-
-echo PGW_CLIENT=$PGW_CLIENT
-echo PGW_SECRET=$PGW_SECRET
-echo PGW_HOST=$PGW_HOST
-
-sudo docker stop webapp_marketplace-$oldVersion
-sudo docker rm webapp_marketplace-$oldVersion
-sudo docker build -t suroor-fashions/webapp-marketplace:$newVersion .
-sudo docker run -p $NON_SECURE_PORT:8080 -p $SECURE_PORT:8443 -d -e SRV_ENV=$1 -e PGW_ENV=$2 -e SESSION_SECRET=$SESSION_SECRET -e AWS_PROFILE=$AWS_PROFILE -e AWS_ACCESS_KEY_ID=$AWS_ACCESS_KEY_ID -e AWS_SECRET_ACCESS_KEY=$AWS_SECRET_ACCESS_KEY -e AWS_REGION=$AWS_REGION -e SECURE_PORT=$SECURE_PORT -e REST_HOST=$AGW_HOST -e REST_PORT=$AGW_PORT -e AUTH_CLIENT=$USER_POOL_CLIENT -e PGW_HOST=$PGW_HOST -e PGW_CLIENT=$PGW_CLIENT -e PGW_SECRET=$PGW_SECRET --name webapp_marketplace-$newVersion suroor-fashions/webapp-marketplace:$newVersion
-sudo docker logs webapp_marketplace-$newVersion --follow
+kubectl run marketplace --image=471388289254.dkr.ecr.us-west-2.amazonaws.com/wharfstreetconsultancy/beyond-test:latest --port=8443 --env="DOMAIN=cluster,SRV_ENV=staging,PGW_ENV=staging,SESSION_SECRET=$SESSION_SECRET,AWS_REGION=$AWS_REGION,SECURE_PORT=$SECURE_PORT,REST_HOST=$AGW_HOST,REST_PORT=$AGW_PORT,AUTH_CLIENT=$USER_POOL_CLIENT,PGW_HOST=$PGW_HOST,PGW_CLIENT=$PGW_CLIENT,PGW_SECRET=$PGW_SECRET"
+# kubectl expose deployment marketplace --port=443 --name=marketplace-https
