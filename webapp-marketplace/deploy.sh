5e5ce3321010feb80669b2cee9540571f04120cf
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2018-04-08 23:53:31 +0100

    Putting JSON on single line.

--- webapp-marketplace/deploy.sh
+++ webapp-marketplace/deploy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
 DEPLOYMENT_NAME=marketplace
-LB_NAME=marketplace-lb
+DEPLOYMENT_LB_NAME=$DEPLOYMENT_NAME-lb
 
 kubectl get deployment | grep $DEPLOYMENT_NAME > /dev/null 2>&1; DEPLOYMENT_EXISTS=$?
 kubectl get service | grep $LB_NAME > /dev/null 2>&1; LB_EXISTS=$?
 
 
 if [ $DEPLOYMENT_EXISTS -eq 0 ]; then
-	echo "Deployment already exists: "$ZONE_DOMAIN
+	echo "Deployment already exists: $DEPLOYMENT_NAME
 
 	kops update cluster --name=$SUB_DOMAIN --state=$KOPS_STATE_STORE --yes
 	echo "OUTCOME: $?"
 #	kubectl rolling-update marketplace-https --image=471388289254.dkr.ecr.us-west-2.amazonaws.com/wharfstreetconsultancy/beyond-test:latest
 	
 else
-	echo "Deployment does not exist."
+	echo "Deployment does not exist: $DEPLOYMENT_NAME"
 
 	SRV_ENV=$(aws s3 cp s3://suroor.fashions.config/marketplace_server.sandbox.env.properties -)
 	PGW_ENV=$(aws s3 cp s3://suroor.fashions.config/payment_gateway.sandbox.env.properties -)
@@ -32,7 +32,7 @@ else
 	PGW_SECRET=$(jq -r '.secret' <<< "${PGW_ENV}")
 	PGW_HOST=$(jq -r '.host' <<< "${PGW_ENV}")
 
-	kubectl run marketplace --image=471388289254.dkr.ecr.us-west-2.amazonaws.com/wharfstreetconsultancy/beyond-test:latest --port=8443 --replicas=3 --env="DOMAIN=cluster,SRV_ENV=sandbox,PGW_ENV=sandbox,SESSION_SECRET=$SESSION_SECRET,AWS_REGION=$AWS_REGION,SECURE_PORT=$SECURE_PORT,REST_HOST=$AGW_HOST,REST_PORT=$AGW_PORT,AUTH_CLIENT=$USER_POOL_CLIENT,PGW_HOST=$PGW_HOST,PGW_CLIENT=$PGW_CLIENT,PGW_SECRET=$PGW_SECRET"
-	kubectl expose deployment marketplace --type=LoadBalancer --port=443 --name=marketplace-lb
+	kubectl run $DEPLOYMENT_NAME --image=471388289254.dkr.ecr.us-west-2.amazonaws.com/wharfstreetconsultancy/beyond-test:latest --port=8443 --replicas=3 --env="DOMAIN=cluster,SRV_ENV=sandbox,PGW_ENV=sandbox,SESSION_SECRET=$SESSION_SECRET,AWS_REGION=$AWS_REGION,SECURE_PORT=$SECURE_PORT,REST_HOST=$AGW_HOST,REST_PORT=$AGW_PORT,AUTH_CLIENT=$USER_POOL_CLIENT,PGW_HOST=$PGW_HOST,PGW_CLIENT=$PGW_CLIENT,PGW_SECRET=$PGW_SECRET"
+	kubectl expose deployment $DEPLOYMENT_NAME --type=LoadBalancer --port=443 --name=$DEPLOYMENT_LB_NAME
 
 fi
