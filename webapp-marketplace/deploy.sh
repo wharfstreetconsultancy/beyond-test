f0b68e3dded98574ec0b25376d3e49f2f635fdd4
Paul Smith <paul.smith1@digital.cabinet-office.gov.uk>
2018-04-14 15:47:02 +0100

    Specified target port.

--- webapp-marketplace/deploy.sh
+++ webapp-marketplace/deploy.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-DEPLOYMENT_NAME="marketplace"
-DEPLOYMENT_LB_NAME="$DEPLOYMENT_NAME-lb"
+# DEPLOYMENT_NAME="marketplace"
+DEPLOYMENT_LB_NAME="${clientName}-lb"
 
-kubectl get deployment | grep $DEPLOYMENT_NAME > /dev/null 2>&1; DEPLOYMENT_EXISTS=$?
+kubectl get deployment | grep $clientName > /dev/null 2>&1; DEPLOYMENT_EXISTS=$?
 kubectl get service | grep $DEPLOYMENT_LB_NAME > /dev/null 2>&1; DEPLOYMENT_LB_EXISTS=$?
 
 
 if [ $DEPLOYMENT_EXISTS -eq 0 ]; then
-	echo "Deployment already exists: $DEPLOYMENT_NAME"
+	echo "Deployment already exists: $clientName"
 
 	kops update cluster --name=$SUB_DOMAIN --state=$KOPS_STATE_STORE --yes
 	echo "OUTCOME: $?"
 #	kubectl rolling-update marketplace-https --image=471388289254.dkr.ecr.us-west-2.amazonaws.com/wharfstreetconsultancy/beyond-test:latest
 	
 else
-	echo "Deployment does not exist: $DEPLOYMENT_NAME"
+	echo "Deployment does not exist: $clientName"
 
 	SRV_ENV=$(aws s3 cp s3://suroor.fashions.config/marketplace_server.sandbox.env.properties -)
 	PGW_ENV=$(aws s3 cp s3://suroor.fashions.config/payment_gateway.sandbox.env.properties -)
@@ -34,7 +34,7 @@ else
 
 	printenv
 
-	kubectl run $DEPLOYMENT_NAME --image=471388289254.dkr.ecr.us-west-2.amazonaws.com/wharfstreetconsultancy/beyond-test:latest --port=8443 --replicas=3 --env="DOMAIN=cluster,SRV_ENV=sandbox,PGW_ENV=sandbox,SESSION_SECRET=$SESSION_SECRET,AWS_REGION=$REGION,SECURE_PORT=$SECURE_PORT,REST_HOST=$AGW_HOST,REST_PORT=$AGW_PORT,AUTH_CLIENT=$USER_POOL_CLIENT,PGW_HOST=$PGW_HOST,PGW_CLIENT=$PGW_CLIENT,PGW_SECRET=$PGW_SECRET"
-	kubectl expose deployment $DEPLOYMENT_NAME --type=LoadBalancer --port=443 --name=$DEPLOYMENT_LB_NAME
+	kubectl run $clientName --image=${dockerRegistry}/${corporation}/${repoDeployable}:${dockerLabel} --port=8443 --replicas=3 --env="DOMAIN=cluster,SRV_ENV=sandbox,PGW_ENV=sandbox,SESSION_SECRET=$SESSION_SECRET,AWS_REGION=$REGION,SECURE_PORT=$SECURE_PORT,REST_HOST=$AGW_HOST,REST_PORT=$AGW_PORT,AUTH_CLIENT=$USER_POOL_CLIENT,PGW_HOST=$PGW_HOST,PGW_CLIENT=$PGW_CLIENT,PGW_SECRET=$PGW_SECRET"
+	kubectl expose deployment $clientName --type=LoadBalancer --port=443 --target-port=8000 --name=$DEPLOYMENT_LB_NAME
 
 fi
